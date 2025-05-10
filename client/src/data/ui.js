dc41a9132512ab2b0a95e5793f5759450846a515
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2025-05-10 17:36:59 +0530

    Navbar updated

--- client/src/data/ui.js
+++ client/src/data/ui.js
@@ -28,3 +28,29 @@ export const features = [
     navTo: "/",
   },
 ];
+
+const mcqQuestions = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    options: ["Berlin", "Madrid", "Paris", "Rome"],
+    answer: "Paris",
+    image: "/Home/f1.png",
+  },
+  {
+    id: 2,
+    question: "Which planet is known as the Red Planet?",
+    options: ["Earth", "Mars", "Jupiter", "Saturn"],
+    answer: "Mars",
+    image: "/Home/f2.png",
+  },
+  {
+    id: 3,
+    question: "What is the largest mammal?",
+    options: ["Elephant", "Blue Whale", "Giraffe", "Shark"],
+    answer: "Blue Whale",
+    image: "/Home/f3.png",
+  },
+];
+
+export { mcqQuestions };
