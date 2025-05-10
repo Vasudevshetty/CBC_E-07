42ee1ea7c2454df549e199b255ed2c5ec351d4f2
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2025-05-10 11:46:58 +0530

    Basic react pages ready

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold mb-2">Login Page</h1>
+    </div>
+  );
+}
+
+export default Login;
