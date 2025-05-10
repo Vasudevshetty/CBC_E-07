ec18f7154be518424e1e7645777a548647340929
Channu17 <channudambal2004@gmail.com>
2025-05-10 12:12:05 +0530

    created the endpoints

--- services/api.py
+++ services/api.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 api = FastAPI()
 
-
+groq_api_key1 = os.getenv("GROQ_API_KEY1")
+groq_api_key2 = os.getenv("GROQ_API_kEY2")
 
 api.add_middleware(
     CORSMiddleware,
@@ -14,6 +18,21 @@ api.add_middleware(
     allow_headers=["*"],
 )
 
+chat_llm = ChatGroq(model= "llama-3.3-70b-versatile", api_key=groq_api_key1)
+llm = ChatGroq(model= "llama-3.3-70b", api_key=groq_api_key2)
+
 @api.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@api.post("/chat")
+def personal_assistant():
+    return {"message": "This is a personal assistant API"}
+
+@api.post("/revision")
+def revision_assistant():
+    return {"message": "This is a revision assistant API"}
+
+@api.post("/carreer")
+def carreer_path():
+    return {"message": "This is a career path assistant API"}
